37be57c9a18edeb7b11bab984225228be3799449
Rasmus Sjoerslev <badedyr@gmail.com>
2017-11-07 21:51:57 +0100

    pre-pended shutdown script with call for each line

--- scripts/save_shutdown_command.ps1
+++ scripts/save_shutdown_command.ps1
@@ -3,12 +3,12 @@ New-Item -Path $packerWindowsDir -ItemType Directory -Force
 
 # build up the clean-up and shutdown command to reset WinRM and shutdown the VM
 $shutdownCmd = @"
-netsh advfirewall firewall delete rule name="WinRM-HTTP"
+call netsh advfirewall firewall delete rule name="WinRM-HTTP"
 
-winrm set winrm/config/service @{AllowUnencrypted="false"}
-winrm set winrm/config/service/auth @{Basic="false"}
+call winrm set winrm/config/service @{AllowUnencrypted="false"}
+call winrm set winrm/config/service/auth @{Basic="false"}
 
-winrm delete winrm/config/Listener?Address=*+Transport=HTTP
+call winrm delete winrm/config/Listener?Address=*+Transport=HTTP
 
 shutdown /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
 "@
