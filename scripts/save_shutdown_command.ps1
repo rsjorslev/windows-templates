bdb251c0954ccfd3c59f058f6612d3c68fa22e91
Rasmus Sjoerslev <badedyr@gmail.com>
2017-11-08 22:04:27 +0100

    extracted variables into file

--- scripts/save_shutdown_command.ps1
+++ scripts/save_shutdown_command.ps1
@@ -10,7 +10,7 @@ call winrm set winrm/config/service/auth @{Basic="false"}
 
 call winrm delete winrm/config/Listener?Address=*+Transport=HTTP
 
-shutdown /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
+shutdown /s /t 900 /f /d p:4:1 /c "Packer Shutdown"
 "@
 
 Set-Content -Path "$($packerWindowsDir)\PackerShutdown.bat" -Value $shutdownCmd
