3fc12b58a31fa1e09a48a674fe16bb6e7c665c6b
Rasmus Sjoerslev <badedyr@gmail.com>
2017-11-10 12:55:28 +0100

    added setupcomplete script

--- scripts/save_shutdown_command.ps1
+++ scripts/save_shutdown_command.ps1
@@ -10,7 +10,16 @@ call winrm set winrm/config/service/auth @{Basic="false"}
 
 call winrm delete winrm/config/Listener?Address=*+Transport=HTTP
 
-shutdown /s /t 900 /f /d p:4:1 /c "Packer Shutdown"
+call shutdown /s /t 120 /f /d p:4:1 /c "Packer Shutdown"
 "@
 
 Set-Content -Path "$($packerWindowsDir)\PackerShutdown.bat" -Value $shutdownCmd
+
+$setupComplete = @"
+set LOCALAPPDATA=%USERPROFILE%\AppData\Local
+set PSExecutionPolicyPreference=Unrestricted
+powershell -File "%systemdrive%\Windows\packer\winrm_ansible.ps1" -EnableCredSSP >"%systemdrive%\Windows\packer\winrm_ansible_log.txt" 2>&1
+"@
+
+New-Item -Path 'C:\Windows\Setup\Scripts' -ItemType Directory -Force
+Set-Content -path "C:\Windows\Setup\Scripts\SetupComplete.cmd" -Value $setupComplete
